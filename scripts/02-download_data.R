433698c8c81d96461d1f176dea8907538a8107cf
Doran Wang <doran.wang@mail.utoronto.ca>
2024-11-26 14:12:09 -0500

    -Update scripts/02-download_data.R

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,60 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from MyAnimeList by using API
+# Author: Doran Wang
+# Date: 26 November 2024
+# Contact: doran.wang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Create an account in MyAnimeList(MAL), then apply the client 
+  #accessing the MAL API.
+# Any other information needed? 
+  # For not being disclosed, I set the client ID in .Renviron. Method: 
+  # 1. install and load package "usethis". 2. run "edit_r_environ()" in console.
+  # or For convenience, you can paste your client ID down below by uncomment the
+  # line of setting CLIENT_ID.
+
 
 
 #### Workspace setup ####
+library(httr)
+library(jsonlite)
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+
+### API setup ###
+# Your MyAnimeList Client ID
+# CLIENT_ID <- "YOUR_CLIENT_ID"
+
+# API URL
+url <- "https://api.myanimelist.net/v2/anime/ranking?ranking_type=favorite&limit=50&fields=mean,popularity,num_list_users,rank"
+
+# Make the GET request
+response <- GET(
+  url,
+  add_headers(`X-MAL-CLIENT-ID` = CLIENT_ID)
+)
+
+# Check if the request was successful
+if (http_status(response)$category == "Success") {
+  # Parse the JSON response
+  anime_data <- fromJSON(content(response, "text"))
+  
+  # Print the parsed response
+  print(anime_data)
+} else {
+  # Print error message
+  print(paste("Error:", http_status(response)$message))
+}
+
+
+
+#### Create data ####
+anime_data <- anime_data$data$node
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_parquet(anime_data, "data/01-raw_data/anime_data.parquet") 
 
          
